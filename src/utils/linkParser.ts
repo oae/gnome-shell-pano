73d7fd313b18c04b1bda7b96ab2b54e452c4f7f7
Alperen Elhan <alperen@elhan.io>
2022-04-24 18:54:17 +0300

    fix: check if title is object or string during parse

--- src/utils/linkParser.ts
+++ src/utils/linkParser.ts
@@ -1,13 +1,13 @@
 import { File, FileCreateFlags } from '@imports/gio2';
 import { ChecksumType, compute_checksum_for_string, PRIORITY_DEFAULT } from '@imports/glib2';
 import { Message, Session } from '@imports/soup3';
-import { XMLParser } from 'fast-xml-parser';
 import { getCachePath, logger } from '@pano/utils/shell';
+import { XMLParser } from 'fast-xml-parser';
 
-const DEFAULT_USER_AGENT =
-  'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36';
+const DEFAULT_USER_AGENT = 'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)';
+
 const session = new Session();
-session.timeout = 2;
+session.timeout = 5;
 
 const decoder = new TextDecoder();
 
@@ -44,7 +44,7 @@ export const getMetaList = (doc: any) => findKey(doc, 'meta');
 export const getDocument = async (url: string): Promise<any> => {
   try {
     const message = Message.new('GET', url);
-    message.request_headers.append('user-Agent', DEFAULT_USER_AGENT);
+    message.request_headers.append('User-Agent', DEFAULT_USER_AGENT);
     const response = await session.send_and_read_async(message, PRIORITY_DEFAULT, null);
 
     if (response == null) {
@@ -76,9 +76,15 @@ export const getDocument = async (url: string): Promise<any> => {
 };
 
 export const getTitle = (doc: any, metaList: any[]): string => {
-  return (
-    metaTagContent(metaList, 'og:title') || metaTagContent(metaList, 'twitter:title') || findKey(doc, 'title')?.[0]
-  );
+  const title = findKey(doc, 'title')?.[0];
+  let fallbackTitle = '';
+  if (title && typeof title === 'object' && '#text' in title) {
+    fallbackTitle = title['#text'];
+  } else if (typeof title === 'string') {
+    fallbackTitle = title;
+  }
+
+  return metaTagContent(metaList, 'og:title') || metaTagContent(metaList, 'twitter:title') || fallbackTitle;
 };
 
 export const getDescription = (metaList: any[]): string => {
@@ -99,7 +105,7 @@ export const getImage = async (metaList: any[]) => {
       }
 
       const message = Message.new('GET', imageUrl);
-      message.request_headers.append('user-agent', DEFAULT_USER_AGENT);
+      message.request_headers.append('User-Agent', DEFAULT_USER_AGENT);
       const response = await session.send_and_read_async(message, PRIORITY_DEFAULT, null);
       if (!response) {
         throw new Error('no response');
