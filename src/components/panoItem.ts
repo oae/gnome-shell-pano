82d6169f83d92cf2332dfb7fde05f3a084aca4df
Alperen Elhan <alperen@elhan.io>
2022-08-20 01:34:18 +0300

    refactor: seperate panoItemHeader into another component

--- src/components/panoItem.ts
+++ src/components/panoItem.ts
@@ -1,5 +1,4 @@
 import {
-  ActorAlign,
   ButtonEvent,
   EVENT_PROPAGATE,
   EVENT_STOP,
@@ -8,14 +7,12 @@ import {
   KEY_KP_Enter,
   KEY_Return,
 } from '@imports/clutter10';
-import { icon_new_for_string } from '@imports/gio2';
 import { MetaInfo } from '@imports/gobject2';
 import { Point } from '@imports/graphene1';
-import { BoxLayout, Button, Icon, Label } from '@imports/st1';
+import { BoxLayout } from '@imports/st1';
 import { registerGObjectClass } from '@pano/utils/gjs';
 import { IPanoItemType } from '@pano/utils/panoItemType';
-import { getCurrentExtension } from '@pano/utils/shell';
-import { formatDistanceToNow } from 'date-fns';
+import { PanoItemHeader } from './panoItemHeader';
 @registerGObjectClass
 export class PanoItem extends BoxLayout {
   static metaInfo: MetaInfo = {
@@ -25,13 +22,8 @@ export class PanoItem extends BoxLayout {
     },
   };
 
-  private itemType: IPanoItemType;
-  private date: Date;
-  private header: BoxLayout;
-  private dateLabel: Label;
-  private dateUpdateIntervalId: any;
+  private header: PanoItemHeader;
   public dbId: number | null;
-
   protected body: BoxLayout;
 
   constructor(dbId: number | null, itemType: IPanoItemType, date: Date) {
@@ -47,96 +39,16 @@ export class PanoItem extends BoxLayout {
     this.connect('key-focus-in', () => this.setSelected(true));
     this.connect('key-focus-out', () => this.setSelected(false));
 
-    this.itemType = itemType;
-    this.date = date;
     this.dbId = dbId;
 
-    this.header = new BoxLayout({
-      style_class: `pano-item-header pano-item-header-${this.itemType.classSuffix}`,
-      vertical: false,
-      x_expand: true,
-    });
-    const titleContainer = new BoxLayout({
-      style_class: 'pano-item-title-container',
-      vertical: true,
-      x_align: ActorAlign.FILL,
-      y_align: ActorAlign.FILL,
-      x_expand: true,
-    });
-    const iconContainer = new BoxLayout({
-      style_class: 'pano-icon-container',
+    this.header = new PanoItemHeader(itemType, date);
+    this.header.connect('on-remove', () => {
+      log(`removed item with id: ${dbId}`);
     });
 
-    iconContainer.add_child(
-      new Icon({
-        gicon: icon_new_for_string(`${getCurrentExtension().path}/icons/${this.itemType.icon}`),
-        style_class: 'pano-icon',
-      }),
-    );
-
-    titleContainer.add_child(
-      new Label({
-        text: this.itemType.title,
-        style_class: 'pano-item-title',
-        x_expand: true,
-      }),
-    );
-
-    this.dateLabel = new Label({
-      text: formatDistanceToNow(this.date, { addSuffix: true }),
-      style_class: 'pano-item-date',
-      x_expand: true,
-      y_expand: true,
+    this.header.connect('on-favorite', () => {
+      log(`favorite item with id: ${dbId}`);
     });
-
-    this.dateUpdateIntervalId = setInterval(() => {
-      this.dateLabel.set_text(formatDistanceToNow(this.date, { addSuffix: true }));
-    }, 60000);
-
-    titleContainer.add_child(this.dateLabel);
-
-    const actionContainer = new BoxLayout({
-      style_class: 'pano-item-actions',
-      x_expand: false,
-      y_expand: true,
-      x_align: ActorAlign.END,
-      y_align: ActorAlign.START,
-    });
-
-    const favoriteIcon = new Icon({
-      icon_name: 'emblem-favorite-symbolic',
-      icon_size: 12,
-    });
-
-    const favoriteButton = new Button({
-      style_class: 'pano-item-favorite-button',
-      child: favoriteIcon,
-    });
-
-    favoriteButton.connect('clicked', () => {
-      log(`favorite item with id: ${this.dbId}`);
-    });
-
-    const removeIcon = new Icon({
-      icon_name: 'window-close-symbolic',
-      icon_size: 12,
-    });
-
-    const removeButton = new Button({
-      style_class: 'pano-item-remove-button',
-      child: removeIcon,
-    });
-
-    removeButton.connect('clicked', () => {
-      log(`removed item with id: ${this.dbId}`);
-    });
-
-    actionContainer.add_child(favoriteButton);
-    actionContainer.add_child(removeButton);
-
-    this.header.add_child(iconContainer);
-    this.header.add_child(titleContainer);
-    this.header.add_child(actionContainer);
 
     this.body = new BoxLayout({
       style_class: 'pano-item-body',
@@ -177,10 +89,5 @@ export class PanoItem extends BoxLayout {
     }
 
     return EVENT_PROPAGATE;
-  }
-
-  override vfunc_destroy(): void {
-    clearInterval(this.dateUpdateIntervalId);
-    super.vfunc_destroy();
   }
 }
---
+++ src/components/panoItemHeader.ts
@@ -0,0 +1,116 @@
+import { ActorAlign } from '@imports/clutter10';
+import { icon_new_for_string } from '@imports/gio2';
+import { MetaInfo } from '@imports/gobject2';
+import { BoxLayout, Button, Icon, Label } from '@imports/st1';
+import { registerGObjectClass } from '@pano/utils/gjs';
+import { IPanoItemType } from '@pano/utils/panoItemType';
+import { getCurrentExtension } from '@pano/utils/shell';
+import { formatDistanceToNow } from 'date-fns';
+
+@registerGObjectClass
+export class PanoItemHeader extends BoxLayout {
+  static metaInfo: MetaInfo = {
+    GTypeName: 'PanoItemHeader',
+    Signals: {
+      'on-remove': {},
+      'on-favorite': {},
+    },
+  };
+
+  private dateUpdateIntervalId: any;
+
+  constructor(itemType: IPanoItemType, date: Date) {
+    super({
+      style_class: `pano-item-header pano-item-header-${itemType.classSuffix}`,
+      vertical: false,
+      x_expand: true,
+    });
+
+    const titleContainer = new BoxLayout({
+      style_class: 'pano-item-title-container',
+      vertical: true,
+      x_align: ActorAlign.FILL,
+      y_align: ActorAlign.FILL,
+      x_expand: true,
+    });
+    const iconContainer = new BoxLayout({
+      style_class: 'pano-icon-container',
+    });
+
+    iconContainer.add_child(
+      new Icon({
+        gicon: icon_new_for_string(`${getCurrentExtension().path}/icons/${itemType.icon}`),
+        style_class: 'pano-icon',
+      }),
+    );
+
+    titleContainer.add_child(
+      new Label({
+        text: itemType.title,
+        style_class: 'pano-item-title',
+        x_expand: true,
+      }),
+    );
+
+    const dateLabel = new Label({
+      text: formatDistanceToNow(date, { addSuffix: true }),
+      style_class: 'pano-item-date',
+      x_expand: true,
+      y_expand: true,
+    });
+
+    this.dateUpdateIntervalId = setInterval(() => {
+      dateLabel.set_text(formatDistanceToNow(date, { addSuffix: true }));
+    }, 60000);
+
+    titleContainer.add_child(dateLabel);
+
+    const actionContainer = new BoxLayout({
+      style_class: 'pano-item-actions',
+      x_expand: false,
+      y_expand: true,
+      x_align: ActorAlign.END,
+      y_align: ActorAlign.START,
+    });
+
+    const favoriteIcon = new Icon({
+      icon_name: 'emblem-favorite-symbolic',
+      icon_size: 12,
+    });
+
+    const favoriteButton = new Button({
+      style_class: 'pano-item-favorite-button',
+      child: favoriteIcon,
+    });
+
+    favoriteButton.connect('clicked', () => {
+      this.emit('on-favorite');
+    });
+
+    const removeIcon = new Icon({
+      icon_name: 'window-close-symbolic',
+      icon_size: 12,
+    });
+
+    const removeButton = new Button({
+      style_class: 'pano-item-remove-button',
+      child: removeIcon,
+    });
+
+    removeButton.connect('clicked', () => {
+      this.emit('on-remove');
+    });
+
+    actionContainer.add_child(favoriteButton);
+    actionContainer.add_child(removeButton);
+
+    this.add_child(iconContainer);
+    this.add_child(titleContainer);
+    this.add_child(actionContainer);
+  }
+
+  override vfunc_destroy(): void {
+    clearInterval(this.dateUpdateIntervalId);
+    super.vfunc_destroy();
+  }
+}
