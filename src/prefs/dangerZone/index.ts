0fd04bef70cb8844c89b364f4bb9a6a973c12ea7
Alperen Elhan <alperen@elhan.io>
2022-09-11 04:54:40 +0300

    feat: add session only mode. closes #57, #14

--- src/prefs/dangerZone/index.ts
+++ src/prefs/dangerZone/index.ts
@@ -1,7 +1,8 @@
 import { PreferencesGroup, PreferencesPage } from '@gi-types/adw1';
+import { ClearHistoryRow } from '@pano/prefs/dangerZone/clearHistory';
+import { SessionOnlyModeRow } from '@pano/prefs/dangerZone/sessionOnlyMode';
 import { registerGObjectClass } from '@pano/utils/gjs';
 import { _ } from '@pano/utils/shell';
-import { ClearHistoryRow } from './clearHistory';
 
 @registerGObjectClass
 export class DangerZonePage extends PreferencesPage {
@@ -12,6 +13,7 @@ export class DangerZonePage extends PreferencesPage {
     });
 
     const dangerZoneGroup = new PreferencesGroup();
+    dangerZoneGroup.add(new SessionOnlyModeRow());
     dangerZoneGroup.add(new ClearHistoryRow());
 
     this.add(dangerZoneGroup);
