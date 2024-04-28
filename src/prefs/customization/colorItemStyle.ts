1cc39eff41fc9e466216ba631a2e4a3b1ac5f3f5
boerdereinar <einardegruyl@gmail.com>
2024-04-28 21:24:50 +0200

    Remove redundant settings

--- src/prefs/customization/colorItemStyle.ts
+++ src/prefs/customization/colorItemStyle.ts
@@ -1,7 +1,7 @@
 import Gio from '@girs/gio-2.0';
 import type { ExtensionBase } from '@girs/gnome-shell/dist/extensions/sharedInternals';
 import { ItemExpanderRow } from '@pano/prefs/customization/itemExpanderRow';
-import { createColorRow, createFontRow } from '@pano/prefs/customization/utils';
+import { createFontRow } from '@pano/prefs/customization/utils';
 import { registerGObjectClass } from '@pano/utils/gjs';
 import { getPanoItemTypes } from '@pano/utils/panoItemType';
 import { getCurrentExtensionSettings, gettext } from '@pano/utils/shell';
@@ -15,26 +15,6 @@ export class ColorItemStyleRow extends ItemExpanderRow {
     super(ext, _('Color Item Style'), _('Change the style of the color item'), getPanoItemTypes(ext).COLOR.iconName);
 
     this.settings = getCurrentExtensionSettings(ext).get_child('color-item');
-
-    // create metadata background color row
-    this.add_row(
-      createColorRow(
-        _('Metadata Background Color'),
-        _('You can change the background color of the metadata'),
-        this.settings,
-        'metadata-bg-color',
-      ),
-    );
-
-    // create metadata text color row
-    this.add_row(
-      createColorRow(
-        _('Metadata Text Color'),
-        _('You can change the text color of the metadata'),
-        this.settings,
-        'metadata-color',
-      ),
-    );
 
     // create metadata font row
     this.add_row(
